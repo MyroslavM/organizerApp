c7bd767eadafe845c2286fe9034b6ebd75540372
MyroslavM <meroslavchuk@gmail.com>
2020-03-15 22:23:27 +0200

    finish component organizer

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../shared/data.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-organizer',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizerComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup
+  constructor(private dateService: DataService ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl("", Validators.required)
+    })
+  }
+  submit(){
+    const {title} = this.form.value
+    console.log(title)
   }
 
 }
