ce8a3ec5577c7fb1d2d192c94cbfa6d932b78da4
MyroslavM <meroslavchuk@gmail.com>
2020-03-16 21:04:03 +0200

    finish all component

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataService} from '../shared/data.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Task, TasksService} from '../shared/tasks.service';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-organizer',
@@ -9,17 +11,43 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class OrganizerComponent implements OnInit {
 
-  form:FormGroup
-  constructor(private dateService: DataService ) { }
+  form: FormGroup
+  tasks: Task[] = []
+
+  constructor(private dateService: DataService,
+              private tasksService: TasksService) {
+  }
 
   ngOnInit() {
+    this.dateService.data.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
     this.form = new FormGroup({
-      title: new FormControl("", Validators.required)
+      title: new FormControl('', Validators.required)
     })
   }
-  submit(){
+
+  submit() {
     const {title} = this.form.value
-    console.log(title)
+
+    const task: Task = {
+      title,
+      date: this.dateService.data.value.format('DD-MM-YYYY')
+    }
+
+    this.tasksService.create(task).subscribe(task => {
+      this.tasks.push(task)
+      this.form.reset()
+    }, err => console.error(err))
+  }
+
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, err => console.error(err))
   }
 
 }
